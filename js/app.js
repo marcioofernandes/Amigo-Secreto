e137b2965ecc84fe917d5d65e028fad527a87115
marcioofernandes <marc.fernandes13@gmail.com>
2024-08-17 23:21:37 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,60 @@
+let amigos = [];
+
+function adicionar () {
+    let amigo = document.getElementById("nome-amigo");
+    if (amigo.value == "") {
+        alert ("Informe o nome do amigo");
+        return;
+    }
+
+    if (amigos.includes (amigo.value)) {
+        alert ("Nome já adicionado, coloque um nome diferente");
+        return;
+    }
+
+    let lista = document.getElementById("lista-amigos");
+
+    amigos.push(amigo.value);  
+
+    if (lista.textContent == "") {
+        lista.textContent = amigo.value; 
+    } else {
+        lista.textContent = lista.textContent + " , " + amigo.value;
+    }
+    amigo.value = ""; 
+}
+
+function sortear () {
+    if (amigos.length < 4) {
+        alert ("Adicione pelo menos 4 amigos");
+        return;
+    }
+    embaralhar (amigos);
+    let listaSorteio = document.getElementById("lista-sorteio");
+
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            listaSorteio.innerHTML = listaSorteio.innerHTML + amigos[i] + " -->" + amigos [0] + "<br>";
+
+        } else {
+            listaSorteio.innerHTML = listaSorteio.innerHTML + amigos[i] + " -->" + amigos [i + 1] + "<br>";
+        }
+        
+        
+    }
+}
+
+// esta parte do código veio pronta de um site para embaralhar os arrays. 
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar () {
+    amigos = []
+    document.getElementById("lista-amigos").innerHTML = "";
+    document.getElementById("lista-sorteio").innerHTML = "";
+
+}
